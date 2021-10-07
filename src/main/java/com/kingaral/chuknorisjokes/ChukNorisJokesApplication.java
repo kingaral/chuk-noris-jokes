636f8b6106be835bb4e3a7dbaa2a62820d0b80a3
“Tokmurzin <mukagali.2002.05@gmail.com>
2021-10-07 19:24:33 +0600

    Initial commit

---
+++ src/main/java/com/kingaral/chuknorisjokes/ChukNorisJokesApplication.java
@@ -0,0 +1,13 @@
+package com.kingaral.chuknorisjokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChukNorisJokesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChukNorisJokesApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/kingaral/chuknorisjokes/services/JokeService.java
@@ -0,0 +1,5 @@
+package com.kingaral.chuknorisjokes.services;
+
+public interface JokeService {
+    String getJoke();
+}
